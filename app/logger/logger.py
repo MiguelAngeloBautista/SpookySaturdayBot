a9bfa7540240b7d041f2dcb443173f04caff182b
MiguelAngeloBautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2025-02-03 14:57:31 +1030

    fix: Fix logging to write to container log

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,9 +1,15 @@
 
-import datetime
-from colorist import BrightColor as BColour
 
-def log(message):
+from colorist import BrightColor as BColour
+import logging
+
+logging.basicConfig(level=logging.INFO, 
+                    format=f"{BColour.BLACK}%(asctime)s{BColour.OFF} {BColour.CYAN}APP{BColour.OFF} %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S")
+
+def log(message: str, level=logging.INFO, ):
     """
     Logs a message to the console with a timestamp.
     """
-    print(f"{BColour.BLACK}{datetime.datetime.now()}{BColour.OFF} {BColour.CYAN}APP{BColour.OFF} {message}")
+    # print(f"{BColour.BLACK}{datetime.datetime.now()}{BColour.OFF} {BColour.CYAN}APP{BColour.OFF} {message}")
+    logging.log(level, message)
