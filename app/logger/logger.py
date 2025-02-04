6d922e40c015d77a332ef54cf763db16382a6cef
MiguelAngeloBautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2025-02-04 15:42:40 +1030

    refactor: Update format for log function

--- app/logger/logger.py
+++ app/logger/logger.py
@@ -1,15 +1,28 @@
+"""
+Logger module for the SpookySaturdayBot application.
+"""
 
-
-from colorist import BrightColor as BColour
 import logging
+from colorist import BrightColor as BColour
 
-logging.basicConfig(level=logging.INFO, 
-                    format=f"{BColour.BLACK}%(asctime)s{BColour.OFF} {BColour.CYAN}APP{BColour.OFF} %(message)s",
+
+logging.basicConfig(level=logging.INFO,
+                    format=f"{BColour.BLACK}%(asctime)s{BColour.OFF}"
+                    f" %(message)s",
                     datefmt="%Y-%m-%d %H:%M:%S")
 
-def log(message: str, level=logging.INFO, ):
+def log(context: str, message: str, level=logging.INFO, ) -> None:
     """
     Logs a message to the console with a timestamp.
+    
+    ### Args:
+        `message (str)`: The message to log.
+        `level (int)`: The logging level.
+        
+    ### Returns:
+        None
     """
-    # print(f"{BColour.BLACK}{datetime.datetime.now()}{BColour.OFF} {BColour.CYAN}APP{BColour.OFF} {message}")
-    logging.log(level, message)
+
+    final_message = f"{BColour.BLUE}{logging.getLevelName(level)}{BColour.OFF} " \
+                    f"\t {BColour.CYAN}{context}{BColour.OFF} {message}"
+    logging.log(level, final_message)
